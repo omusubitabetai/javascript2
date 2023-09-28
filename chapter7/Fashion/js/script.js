d8d4a43364ab54212bc9741d86649917844d61c4
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2023-09-28 10:29:50 +0000

    7−9から7-11まで

--- chapter7/Fashion/js/script.js
+++ chapter7/Fashion/js/script.js
@@ -56,21 +56,57 @@ window.addEventListener('load', () => {
 /*
 画像ギャラリー
 ================================================ */
-/* ----- ↓↓ 追加 ↓↓ ----- */
 const mainImage = document.querySelector('.gallery-image img');
 const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
-
-//for(let i = 0; i < thumbImages.length; i++) {
-//  thumbImages[i].addEventListener('mouseover', (event) => {
-//      mainImage.src = event.target.src;
-//      mainImage.animate({opacity: [0, 1]}, 500);
-// });
-//}
 
 thumbImages.forEach((thumbImage)=>{
   thumbImage.addEventListener('mouseover', (event) => {
     mainImage.src = event.target.src;
     mainImage.animate({opacity: [0, 1]}, 500);
+  });
+});
+
+/*
+スライドメニュー
+================================================ */
+/* ----- ↓↓ 追加 ↓↓ ----- */
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const menuPanel = document.querySelector('#menu-panel');
+const menuItems = document.querySelectorAll('#menu-panel li');
+const menuOptions = {
+  duration: 1400,
+  easing: 'ease',
+  fill: 'forwards',
+};
+
+// メニューを開く
+menuOpen.addEventListener('click', () => {
+  menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+
+  // リンクをひとつずつ順に表示
+  menuItems.forEach((menuItem, index) => {
+    //console.log(`${index}番目のリスト`);
+    menuItem.animate(
+      {
+        opacity: [0, 1],
+        translate: ['2rem', 0],
+      },
+      {
+        duration: 2400,
+        delay: 300 * index,
+        easing: 'ease',
+        fill: 'forwards',
+      }
+    );
+  });
+});
+
+// メニューを閉じる
+menuClose.addEventListener('click', () => {
+  menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+  menuItems.forEach((menuItem) => {
+    menuItem.animate({opacity: [1, 0]}, menuOptions);
   });
 });
 /* ----- ↑↑ 追加 ↑↑ ----- */
